57ee7a828d3437bdcec5ae57bf9e758d79302348
VedaAI Dev <dev@vedaai.com>
2026-03-22 21:50:16 +0530

    quick fix

--- vedaAiBackend/src/config/dotenv.ts
+++ vedaAiBackend/src/config/dotenv.ts
@@ -3,7 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv()
 
 export const PORT = process.env.PORT
-export const DB_URL = process.env.MONGO_URI
+export const DB_URL = process.env.DB_URL
 export const REDIS_URL = process.env.REDIS_URL
 export const CLIENT_URL = process.env.CLIENT_URL
 export const NODE_ENV = process.env.NODE_ENV
